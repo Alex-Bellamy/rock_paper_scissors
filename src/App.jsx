fe3e9c9a6cc83a9dc56a29c5fb14921f2ba0cbcc
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-04 11:36:53 +0200

    adding rule specs and implementation code to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ class App extends Component {
   return (
     <div>
       <h1 id="title">Rock.Paper.Scissors</h1>
+      <h2 id="sub-title">The rules are simple...</h2>
+      <p id="rules-1">1. rock smashes scissors.</p>
+      <p id="rules-2">2. scissors slice up paper.</p>
+      <p id="rules-3">3. paper smothers rock.</p>
       <img id="rock" src={require('./assets/rock.png')} alt="rock" />
       <img id="paper" src={require('./assets/paper.png')} alt="paper" />
       <img id="scissors" src={require('./assets/scissors.png')} alt="scissors" />
