55530ba7d117985e9cf68c15a3ba042ffd9a5cd0
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-05 00:01:48 +0200

    assigning buttons to game play symbols, onclick events tothe aforementioned and startGame function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  
-  render() {
-  return (
-    <div>
-      <h1 id="title">Rock.Paper.Scissors</h1>
-      <h2 id="sub-title">The rules are simple...</h2>
-      <p id="rules-1">1. rock smashes scissors.</p>
-      <p id="rules-2">2. scissors slice up paper.</p>
-      <p id="rules-3">3. paper smothers rock.</p> <br/> <br/>
-      <img id="rock" src={require('./assets/rock.png')} alt="rock" /> 
-      <img id="paper" src={require('./assets/paper.png')} alt="paper" />
-      <img id="scissors" src={require('./assets/scissors.png')} alt="scissors" />
-      <p><strong>Choose your weapon!</strong></p>
+
+startGame = (e) => {
+  const playerWeapon = e.target.id;
+}
+
+render() {
+return (
+  <div>
+  <h1 id="title">Rock.Paper.Scissors</h1>
+   <h2 id="sub-title">The rules are simple...</h2>
+   <p id="rules-1">1. rock smashes scissors.</p>
+   <p id="rules-2">2. scissors slice up paper.</p>
+   <p id="rules-3">3. paper smothers rock.</p> <br/> <br/>
+   <button onClick={this.startGame.bind(this)}> <img id="rock" src={require('./assets/rock.png')} alt="rock" /> </button>
+   <button onClick={this.startGame.bind(this)}> <img id="paper" src={require('./assets/paper.png')} alt="paper" /> </button>
+   <button onClick={this.startGame.bind(this)}> <img id="scissors" src={require('./assets/scissors.png')} alt="scissors" /> </button>
+   <p><strong>Choose your weapon!</strong></p>
  </div>
   )
  }
