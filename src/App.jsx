d069f2f41ef5ca2ebb7d84c33fa78ad543ec2930
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-05 17:48:55 +0200

    writing Outcome function to ascrtain whether player or computer is the winner and formatting code

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const weapons = ["rock", "paper", "scissors"];
 class App extends Component {
+  startGame = (e) => {
+    const playerWeapon = e.target.id;
+    const computerWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+    const outcome = this.Outcome(playerWeapon, computerWeapon);
+  };
 
-startGame = (e) => {
-  const playerWeapon = e.target.id;
-  const computerWeapon = weapons[Math.floor(Math.random() * weapons.length)];
-}
+  Outcome = (playerWeapon, computerWeapon) => {
+    if (playerWeapon == computerWeapon) {
+      return "Stalemate, draw";
+    }
 
-render() {
-return (
-  <div>
-  <h1 id="title">Rock.Paper.Scissors</h1>
-   <h2 id="sub-title">The rules are simple...</h2>
-   <p id="rules-1">1. rock smashes scissors.</p>
-   <p id="rules-2">2. scissors slice up paper.</p>
-   <p id="rules-3">3. paper smothers rock.</p> <br/> <br/>
-   <button onClick={this.startGame.bind(this)}> <img id="rock" src={require('./assets/rock.png')} alt="rock" /> </button>
-   <button onClick={this.startGame.bind(this)}> <img id="paper" src={require('./assets/paper.png')} alt="paper" /> </button>
-   <button onClick={this.startGame.bind(this)}> <img id="scissors" src={require('./assets/scissors.png')} alt="scissors" /> </button>
-   <p><strong>Choose your weapon!</strong></p>
- </div>
-  )
- }
+    if (
+      (playerWeapon == "rock" && computerWeapon == "scissors") ||
+      (playerWeapon == "paper" && computerWeapon == "rock") ||
+      (playerWeapon == "scissors" && computerWeapon == "paper")
+    ) {
+      return "Player is victorious";
+    } else {
+      return "Whipped by a computer";
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 id="title">Rock.Paper.Scissors</h1>
+        <h2 id="sub-title">The rules are simple...</h2>
+        <p id="rules-1">1. rock smashes scissors.</p>
+        <p id="rules-2">2. scissors slice up paper.</p>
+        <p id="rules-3">3. paper smothers rock.</p> <br /> <br />
+        <button onClick={this.startGame.bind(this)}>
+          {" "}
+          <img id="rock" src={require("./assets/rock.png")} alt="rock" />{" "}
+        </button>
+        <button onClick={this.startGame.bind(this)}>
+          {" "}
+          <img
+            id="paper"
+            src={require("./assets/paper.png")}
+            alt="paper"
+          />{" "}
+        </button>
+        <button onClick={this.startGame.bind(this)}>
+          {" "}
+          <img
+            id="scissors"
+            src={require("./assets/scissors.png")}
+            alt="scissors"
+          />{" "}
+        </button>
+        <p>
+          <strong>Choose your weapon!</strong>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
